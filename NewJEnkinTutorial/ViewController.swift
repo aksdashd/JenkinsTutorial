39ba397bd9121fdbfae8be9113a9566cbcad781e
Madhumita Maji <mmaji1@its.jnj.com>
2017-01-10 23:29:33 +0530

    Initial Commit

---
+++ NewJEnkinTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NewJEnkinTutorial
+//
+//  Created by Sahitya Tarumani on 10/01/17.
+//  Copyright © 2017 Sahitya Tarumani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        NSLog("%@","akash")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
